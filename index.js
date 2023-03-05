802eb36e4c89dae8dc9de356536337a0c327500b
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-04 20:10:32 -0700

    Modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Paul",
+    streetAddress: "4341 Sue Rd"
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    return {
+        ...employee,
+        [name]: streetAddress,
+    };
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = "12 Broadway"
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name, value) {
+    const newEmployee = {...employee};
+    newEmployee[name] = value;
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
+
+
